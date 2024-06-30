e82df4543f3a7b6ed6fe7f563de38770a3796774
Yury Borets <udborets@gmail.com>
2024-06-30 18:09:11 +0300

    implement densenet class

--- models/DenseNet.py
+++ models/DenseNet.py
@@ -1,19 +1,21 @@
+from typing import Optional
 import torch.nn as nn
 import torch
 
 
-class _TransitionLayer(nn.Module):
+class _TransitionLayer(nn.ModuleList):
     def __init__(
         self,
-        in_features: int,
         in_channels,
-        out_channels: int,
+        out_channels: Optional[int] = None,
         *args,
         **kwargs,
     ):
-        super().__init__(*args, **kwards)
+        super().__init__(*args, **kwargs)
+        if out_channels is None:
+            out_channels = in_channels
+        self.out_channels = out_channels
         self.act = nn.ReLU(inplace=True)
-        self.in_features = in_features
         self.in_channels = in_channels
         self.bn = nn.BatchNorm2d(in_channels)
         self.conv = nn.Conv2d(
@@ -36,27 +38,95 @@ class _TransitionLayer(nn.Module):
         return out
 
 
-class _DenseBlock(nn.Module):
-    pass
+class _DenseBlock(nn.ModuleList):
+    def __init__(
+        self,
+        in_channels: int,
+        conv_num: int,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        for i in range(conv_num):
+            conv = nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=in_channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+            bn = nn.BatchNorm2d(in_channels)
+            act = nn.ReLU(inplace=True)
+            self.add_module(f"layer_{i + 1}", nn.Sequential(bn, act, conv))
+
+    def forward(self, x: torch.Tensor):
+        outputs = [x]
+        for layer in self:
+            out = layer(x)
+            outputs.append(out)
+        return torch.concat(outputs, 1)
 
 
-class DenseNet(nn.Module):
+# DenseNet(5, 3, 256, [2, 2, 3], 32)
+class DenseNet(nn.ModuleList):
     def __init__(
         self,
         num_classes: int,
+        in_channels: int,
+        in_features: int,
         conv_num_in_blocks,
         stack_channels: int,
         *args,
         **kwargs,
     ) -> None:
         super().__init__(*args, **kwargs)
+        self.upsample = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=stack_channels,
+            kernel_size=1,
+            stride=1,
+        )
         self.num_classes = num_classes
-        self.classifier = nn.Linear()
-        # for i, num_conv_layers in enumerate(conv_num_in_blocks):
-        # self.add_module("conv_1")
+
+        num_channels = stack_channels
+        for i in range(len(conv_num_in_blocks) - 1):
+            dense = _DenseBlock(num_channels, conv_num_in_blocks[i])
+            self.add_module(f"dense_{i + 1}", dense)
+            num_channels += num_channels * conv_num_in_blocks[i]
+            self.add_module(
+                f"transition_{i + 1}",
+                _TransitionLayer(
+                    in_channels=num_channels,
+                ),
+            )
+        dense = _DenseBlock(num_channels, conv_num_in_blocks[-1])
+        num_channels += num_channels * conv_num_in_blocks[-1]
+        self.add_module(f"dense_{len(conv_num_in_blocks)}", dense)
+        self.add_module(
+            "downsample",
+            nn.Conv2d(
+                in_channels=num_channels,
+                out_channels=num_classes,
+                kernel_size=1,
+                stride=1,
+            ),
+        )
+        self.add_module(
+            "global_avg_pool",
+            nn.AvgPool2d(kernel_size=in_features // 2 ** (len(conv_num_in_blocks) - 1)),
+        )
+        self.add_module("flatten", nn.Flatten())
+
+    def forward(self, x: torch.Tensor):
+        out = x
+        for layer in self:
+            out = layer(out)
+            print(layer, out.shape)
+        return out
 
     @staticmethod
-    def from_config(config_path: str) -> DenseNet:
+    def from_config(config_path: str) -> "DenseNet":
         pass
 
     @staticmethod
